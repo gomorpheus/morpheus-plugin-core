9f02830a1dd5f27de8e3f5661c933f7c02ffe81a
Mike Truso <mike@agileorbit.com>
2020-04-17 10:50:38 -0500

    extend MorpheusModel

--- morpheus-core/src/main/java/com/morpheusdata/model/Task.java
+++ morpheus-core/src/main/java/com/morpheusdata/model/Task.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-public class Task {
+public class Task extends MorpheusModel {
 	Account account;
 	String name;
 	String code;
