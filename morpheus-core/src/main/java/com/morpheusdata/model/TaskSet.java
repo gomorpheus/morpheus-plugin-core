b69c5f5fa3aa07b796e04062e561bd0946ce7f39
Mike Truso <mike@agileorbit.com>
2020-04-17 10:50:38 -0500

    extend MorpheusModel

--- morpheus-core/src/main/java/com/morpheusdata/model/TaskSet.java
+++ morpheus-core/src/main/java/com/morpheusdata/model/TaskSet.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-public class TaskSet {
+public class TaskSet extends MorpheusModel {
 	static boolean searchable = false;
 
 //	static transients = ['taskIds']
