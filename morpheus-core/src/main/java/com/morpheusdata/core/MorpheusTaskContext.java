473b8a32f4448ef4610e85e75a80e7ef059262b0
Mike Truso <mike@agileorbit.com>
2020-04-20 11:28:47 -0500

    reactive types

--- morpheus-core/src/main/java/com/morpheusdata/core/MorpheusTaskContext.java
+++ morpheus-core/src/main/java/com/morpheusdata/core/MorpheusTaskContext.java
@@ -1,7 +1,9 @@
 package com.morpheusdata.core;
 
 import com.morpheusdata.model.TaskType;
+import io.reactivex.Single;
 
 public interface MorpheusTaskContext {
-	TaskType createTask(TaskType taskType);
+	Single<TaskType> createTask(TaskType taskType);
+	Single<Void> disableTask(String code);
 }
--- morpheus-taskset-plugin/src/main/groovy/com/morpheusdata/taskset/MikeTaskService.groovy
+++ morpheus-taskset-plugin/src/main/groovy/com/morpheusdata/taskset/MikeTaskService.groovy
@@ -7,6 +7,7 @@ import com.morpheusdata.model.Task
 
 class MikeTaskService implements ExecutableTaskInterface {
 
+	@Override
 	Map executeLocalTask(Task task, Map opts, Container container, ComputeServer server) {
 		def taskOption = task.taskOptions.find { it.optionType.code == 'mikeTaskText' }
 		String data = taskOption?.value
@@ -15,5 +16,35 @@ class MikeTaskService implements ExecutableTaskInterface {
 				data   : data,
 				output : data.reverse()
 		]
+	}
+
+	@Override
+	Map executeServerTask(ComputeServer server, Task task, Map opts) {
+		return null
+	}
+
+	@Override
+	Map executeServerTask(ComputeServer server, Task task) {
+		return null
+	}
+
+	@Override
+	Map executeContainerTask(Container container, Task task, Map opts) {
+		return null
+	}
+
+	@Override
+	Map executeContainerTask(Container container, Task task) {
+		return null
+	}
+
+	@Override
+	Map executeRemoteTask(Task task, Map opts, Container container, ComputeServer server) {
+		return null
+	}
+
+	@Override
+	Map executeRemoteTask(Task task, Container container, ComputeServer server) {
+		return null
 	}
 }
