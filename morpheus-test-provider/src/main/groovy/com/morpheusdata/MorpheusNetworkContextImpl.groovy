3904f56754bed8b2644ec3f2d8af5a44196ec6ba
Mike Truso <mike@agileorbit.com>
2020-04-21 15:55:19 -0500

    fix test provider

--- morpheus-test-provider/src/main/groovy/com/morpheusdata/MorpheusNetworkContextImpl.groovy
+++ morpheus-test-provider/src/main/groovy/com/morpheusdata/MorpheusNetworkContextImpl.groovy
@@ -9,201 +9,223 @@ import com.morpheusdata.model.NetworkDomain
 import com.morpheusdata.model.NetworkDomainRecord
 import com.morpheusdata.model.NetworkPool
 import com.morpheusdata.model.NetworkPoolIp
+import com.morpheusdata.model.NetworkPoolRange
 import com.morpheusdata.model.NetworkPoolServer
+import io.reactivex.Single
 
 class MorpheusNetworkContextImpl implements MorpheusNetworkContext {
     @Override
-    void updateNetworkPoolStatus(NetworkPoolServer poolServer, String status, String message) {
+	Single<Void> updateNetworkPoolStatus(NetworkPoolServer poolServer, String status, String message) {
 
     }
 
 	@Override
-	void addMissingPools(Long poolServerId, List chunkedAddList) {
-
-	}
-
-	@Override
-	void updateMatchedPools(Long poolServerId, List chunkedUpdateList) {
-
-	}
-
-	@Override
-	void removeMissingPools(Long poolServerId, List removeList) {
-
-	}
-
-	@Override
-	List<NetworkPool> getNetworkPoolByPoolServer(NetworkPoolServer poolServer) {
+	Single<List<NetworkPool>> getNetworkPoolsByNetworkPoolServer(NetworkPoolServer poolServer) {
 		return null
 	}
 
 	@Override
-	void removeMissingPools(Long poolServerId, List removeList) {
-
-	}
-
-	@Override
-	List getNetworkPoolByNetworkPool(NetworkPool pool) {
+	Single<List<NetworkPool>> getNetworkPoolsByNetworkPoolServer(NetworkPoolServer poolServer, String property) {
 		return null
 	}
 
 	@Override
-	void addMissingIps(NetworkPool pool, List addList) {
-
-	}
-
-	@Override
-	void updateMatchedIps(NetworkPool pool, List updateList) {
-
-	}
-
-	@Override
-	void removeMissingIps(NetworkPool pool, List removeList) {
-
-	}
-
-	@Override
-	void removePoolIp(NetworkPool networkPool, NetworkPoolIp ipAddress) {
-
-	}
-
-	@Override
-	NetworkPoolServer getPoolServerByAccountIntegration(AccountIntegration integration) {
+	Single<List<NetworkPool>> getNetworkPoolsByNetworkPoolServerJoin(NetworkPoolServer poolServer, String joinProperty) {
 		return null
 	}
 
 	@Override
-	NetworkPoolServer getPoolServerById(Long id) {
+	Single<List> getModelProperties(NetworkPool pool, List<String> joinProperties) {
 		return null
 	}
 
 	@Override
-	List<NetworkDomain> getNetworkDomainByTypeAndRefId(String refType, Long refId) {
+	Single<Void> removeMissingPools(Long poolServerId, List removeList) {
+
+	}
+
+	@Override
+	Single<Void> removeMissingIps(NetworkPool pool, List removeList) {
+
+	}
+
+	@Override
+	Single<Void> removePoolIp(NetworkPool networkPool, NetworkPoolIp ipAddress) {
+
+	}
+
+	@Override
+	Single<NetworkPoolServer> getPoolServerByAccountIntegration(AccountIntegration integration) {
 		return null
 	}
 
 	@Override
-	List<NetworkPool> getPools(NetworkPoolServer poolServer) {
+	Single<NetworkPoolServer> getPoolServerById(Long id) {
+		return null
+	}
+
+	@Override
+	Single<List<NetworkDomain>> getNetworkDomainByTypeAndRefId(String refType, Long refId) {
+		return null
+	}
+
+	@Override
+	Single<List<NetworkPool>> getPools(NetworkPoolServer poolServer) {
 		return null
 	}
 
     @Override
-    NetworkDomainRecord saveDomainRecord(NetworkDomainRecord domainRecord) {
+	Single<NetworkDomainRecord> saveDomainRecord(NetworkDomainRecord domainRecord) {
         return null
     }
 
 	@Override
-	NetworkDomainRecord saveDomainRecord(NetworkDomainRecord domainRecord, Map opts) {
+	Single<NetworkDomainRecord> saveDomainRecord(NetworkDomainRecord domainRecord, Map opts) {
 		return null
 	}
 
 	@Override
-    NetworkPoolIp saveNetworkPoolIp(NetworkPoolIp poolIp) {
-        return null
-    }
-
-	@Override
-	NetworkPoolIp saveNetworkPoolIp(NetworkPoolIp poolIp, NetworkPool networkPool) {
+	Single<NetworkPoolIp> save(NetworkPoolIp poolIp) {
 		return null
 	}
 
 	@Override
-	NetworkPoolIp saveNetworkPoolIp(NetworkPoolIp poolIp, NetworkPool networkPool, Map opts) {
+	Single<NetworkPoolIp> save(NetworkPoolIp poolIp, NetworkPool networkPool) {
 		return null
 	}
 
 	@Override
-	NetworkPoolIp getNetworkIp(NetworkPool networkPool, String assignedType, Long assignedId, Long subAssignedId) {
+	Single<NetworkPoolIp> save(NetworkPoolIp poolIp, NetworkPool networkPool, Map opts) {
 		return null
 	}
 
 	@Override
-	NetworkDomain getContainerNetworkDomain(Container container) {
+	Single<NetworkPoolIp> getNetworkIp(NetworkPool networkPool, String assignedType, Long assignedId, Long subAssignedId) {
 		return null
 	}
 
 	@Override
-	String getComputeServerExternalFqdn(ComputeServer computeServer) {
+	Single<NetworkDomain> getContainerNetworkDomain(Container container) {
 		return null
 	}
 
 	@Override
-	String getContainerExternalIp(Container container) {
+	Single<String> getComputeServerExternalFqdn(ComputeServer computeServer) {
 		return null
 	}
 
 	@Override
-	String getContainerExternalFqdn(Container container) {
+	Single<String> getContainerExternalIp(Container container) {
 		return null
 	}
 
 	@Override
-	NetworkPoolIp loadNetworkPoolIp(NetworkPool pool, String ipAddress) {
+	Single<String> getContainerExternalFqdn(Container container) {
 		return null
 	}
 
 	@Override
-	List getNetworkDomainByDomainAndRecordType(NetworkDomain domain, String recordType) {
+	Single<NetworkPoolIp> loadNetworkPoolIp(NetworkPool pool, String ipAddress) {
 		return null
 	}
 
 	@Override
-	void updateMatchedDomainRecords(Long poolServerId, NetworkDomain domain, String recordType, List updateList) {
-
-	}
-
-	@Override
-	void removeMissingDomainRecords(Long poolServerId, NetworkDomain domain, String recordType, List removeList) {
-
-	}
-
-	@Override
-	String acquireLock(String name, Map opts) {
+	Single<List> getNetworkDomainByDomainAndRecordType(NetworkDomain domain, String recordType) {
 		return null
 	}
 
 	@Override
-	String releaseLock(String name, Map opts) {
+	Single<Void> removeMissingDomainRecords(Long poolServerId, NetworkDomain domain, String recordType, List removeList) {
+
+	}
+
+	@Override
+	Single<String> acquireLock(String name, Map opts) {
 		return null
 	}
 
 	@Override
-	void addMissingDomainRecords(Long poolServerId, NetworkDomain domain, String recordType, List addList) {
-
-	}
-
-	@Override
-	void addMissingZones(Long poolServerId, List addList) {
-
-	}
-
-	@Override
-	void updateMatchedZones(Long poolServerId, List updateList) {
-
-	}
-
-	@Override
-	void removeMissingZones(Long poolServerId, List removeList) {
-
-	}
-
-	@Override
-	List getNetworkDomainByOwner(Account account) {
-
-	}
-
-	@Override
-	NetworkDomainRecord getNetworkDomainRecordByNetworkDomainAndContainerId(NetworkDomain domainMatch, Long containerId) {
+	Single<String> releaseLock(String name, Map opts) {
 		return null
 	}
 
 	@Override
-	void deleteNetworkDomainAndRecord(NetworkDomain networkDomain, NetworkDomainRecord domainRecord) {
+	Single<Void> createSyncedNetworkDomain(Long poolServerId, List addList) {
+		return null
+	}
+
+	@Override
+	Single<List<NetworkDomain>> findNetworkDomainsByPoolServerAndExternalIdsOrNames(NetworkPoolServer poolServer, List externalIds, List nameList) {
+		return null
+	}
+
+	@Override
+	Single<List<NetworkPoolIp>> getNetworkPoolIpsByNetworkPoolAndExternalIdOrIpAddress(NetworkPool pool, List externalIds, List ipAddresses) {
+		return null
+	}
+
+	@Override
+	Single<Void> saveAllNetworkDomains(List<NetworkDomain> domainsToSave) {
+		return null
+	}
+
+	@Override
+	Single<Void> removeMissingZones(Long poolServerId, List removeList) {
 
 	}
 
 	@Override
-	NetworkDomain getServerNetworkDomain(ComputeServer computeServer) {
+	Single<List> getNetworkDomainByOwner(Account account) {
+
+	}
+
+	@Override
+	Single<NetworkDomainRecord> getNetworkDomainRecordByNetworkDomainAndContainerId(NetworkDomain domainMatch, Long containerId) {
+		return null
+	}
+
+	@Override
+	Single<Void> deleteNetworkDomainAndRecord(NetworkDomain networkDomain, NetworkDomainRecord domainRecord) {
+
+	}
+
+	@Override
+	Single<NetworkDomain> getServerNetworkDomain(ComputeServer computeServer) {
+		return null
+	}
+
+	@Override
+	Single<NetworkPool> save(NetworkPool networkPool) {
+		return null
+	}
+
+	@Override
+	Single<NetworkPoolRange> save(NetworkPoolRange networkPoolRange) {
+		return null
+	}
+
+	@Override
+	Single<NetworkDomainRecord> save(NetworkDomainRecord domainRecord) {
+		return null
+	}
+
+	@Override
+	Single<Void> saveAll(List<NetworkDomainRecord> domainRecords) {
+		return null
+	}
+
+	@Override
+	Single<Void> save(NetworkPool networkPool, List<NetworkPoolRange> ranges) {
+		return null
+	}
+
+	@Override
+	Single<Map<String, NetworkPool>> findNetworkPoolsByPoolServerAndExternalIds(NetworkPoolServer pool, List externalIds) {
+		return null
+	}
+
+	@Override
+	Single<Map<String, NetworkDomainRecord>> findNetworkDomainRecordByNetworkDomainAndTypeAndExternalIds(NetworkDomain domain, String recordType, List externalIds) {
 		return null
 	}
 }
