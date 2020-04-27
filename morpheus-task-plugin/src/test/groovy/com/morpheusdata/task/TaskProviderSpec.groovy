13a20828b32945fd1f780c1a1548d611f045ae9f
Mike Truso <mike@agileorbit.com>
2020-04-27 10:55:46 -0500

    code cleanup

--- morpheus-task-plugin/src/test/groovy/com/morpheusdata/task/TaskProviderSpec.groovy
+++ morpheus-task-plugin/src/test/groovy/com/morpheusdata/task/TaskProviderSpec.groovy
@@ -3,6 +3,8 @@ package com.morpheusdata.task
 import com.morpheusdata.MorpheusContextImpl
 import com.morpheusdata.core.ExecutableTaskInterface
 import com.morpheusdata.core.MorpheusContext
+import com.morpheusdata.core.TaskProvider
+import com.morpheusdata.model.OptionType
 import com.morpheusdata.model.Task
 import spock.lang.Shared
 import spock.lang.Specification
@@ -31,6 +33,13 @@ class TaskProviderSpec extends Specification {
 
 	void "service instantiated"() {
 		expect:
+		provider instanceof TaskProvider
 		provider.service instanceof ExecutableTaskInterface
+	}
+
+	void "takes a list of OptionType"() {
+		expect:
+		provider.optionTypes.size() == 1
+		provider.optionTypes[0].inputType == OptionType.InputType.TEXT
 	}
 }
