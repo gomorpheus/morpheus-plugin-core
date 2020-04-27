ce9d03a6b86ac988c1e2c50b14cccca4cd2172c2
Mike Truso <mike@agileorbit.com>
2020-04-27 16:43:39 -0500

    initial custom tab plugin

---
+++ morpheus-tab-plugin/src/main/groovy/com/morpheusdata/tab/TabPlugin.groovy
@@ -0,0 +1,16 @@
+package com.morpheusdata.tab
+
+import com.morpheusdata.core.Plugin
+
+class TabPlugin extends Plugin {
+
+	@Override
+	void initialize() {
+
+	}
+
+	@Override
+	void onDestroy() {
+
+	}
+}
