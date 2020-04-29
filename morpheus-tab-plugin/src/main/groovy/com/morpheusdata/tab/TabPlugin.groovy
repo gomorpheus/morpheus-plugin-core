7605e0d07ab412fd692cf4535e8bf583bbeb8c9f
Mike Truso <mike@agileorbit.com>
2020-04-29 15:30:37 -0500

    expose child first class loader for plugins

--- morpheus-tab-plugin/src/main/groovy/com/morpheusdata/tab/TabPlugin.groovy
+++ morpheus-tab-plugin/src/main/groovy/com/morpheusdata/tab/TabPlugin.groovy
@@ -6,7 +6,9 @@ class TabPlugin extends Plugin {
 
 	@Override
 	void initialize() {
-
+		CustomTabProvider customTabProvider = new CustomTabProvider(this, morpheusContext)
+		this.pluginProviders.put(customTabProvider.providerCode, customTabProvider)
+		this.setName("Custom Tabs")
 	}
 
 	@Override
