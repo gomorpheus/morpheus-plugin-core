b6df11375b04025628cbbd6acd8d0fd62cb96df3
Mike Truso <mike@agileorbit.com>
2020-05-13 15:37:07 -0500

    more instance properties

--- morpheus-plugin-api/src/main/java/com/morpheusdata/model/Instance.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/Instance.java
@@ -1,11 +1,47 @@
 package com.morpheusdata.model;
 
 import java.util.Collection;
+import java.util.Date;
 
 public class Instance extends MorpheusModel {
 	private String uuid;
 	private String name;
 	private String description;
+	public String instanceTypeName;
+	public String instanceTypeCode;
+	public String provisionType;
+	public String layoutId;
+	public String layoutCode;
+	public String layoutName;
+	public String instanceVersion;
+
+	public String plan;
+	public String displayName;
+	public String environmentPrefix;
+	public String hostname;
+	//	public String domainName;
+//	public String assignedDomainName;
+	public Boolean firewallEnabled;
+	public String status;
+	public String userStatus;
+	public String scheduleStatus;
+	public String networkLevel;
+	public String instanceLevel;
+	public String deployGroup;
+	public String instanceContext;
+	public Boolean autoScale;
+	public String statusMessage;
+	public Date expireDate;
+	public String tags;
+	public Long maxStorage;
+	public Long maxMemory;
+	public Long maxCores;
+	public Long configId;
+	public String configGroup;
+	public String configRole;
+	//	public String ports;
+	public String serviceUsername;
+	public String servicePassword;
 
 
 	private Collection<Workload> workloads;
