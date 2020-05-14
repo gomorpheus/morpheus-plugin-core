d2a614d69b1fdf9f5f6e7ac1ac1bd7e1fea715f7
Eric Helgeson <erichelgeson@gmail.com>
2020-05-14 13:40:57 -0500

    Fixup Permission checks

--- morpheus-plugin-api/src/main/java/com/morpheusdata/model/Permission.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/Permission.java
@@ -16,20 +16,45 @@ public class Permission extends MorpheusModel {
 	public String code;
 	public List<AccessType> availableAccessTypes;
 
-	public Permission(String name, String code) {
+
+	public Permission(String name, String code, List<AccessType> availableAccessTypes) {
 		this.name = name;
 		this.code = code;
+		this.availableAccessTypes = availableAccessTypes;
+	}
+
+	public Permission(String code, List<AccessType> availableAccessTypes) {
+		this.name = code;
+		this.code = code;
+		this.availableAccessTypes = availableAccessTypes;
 	}
 
 	public Map<String, String> asMap() {
 		HashMap<String, String> map = new HashMap<>();
-		map.put(this.name, this.code);
+		List<String> types = this.typesAsString();
+		String highestType = types.get(types.size() - 1);
+		map.put(this.code, highestType);
 		return map;
 	}
 
-	public static Permission build(String name, String code) {
-		return new Permission(name, code);
+	public List<String> typesAsString() {
+		List<String> types = new ArrayList<>();
+		for(AccessType accessType: this.availableAccessTypes) {
+			types.add(accessType.name());
+		}
+		return types;
 	}
+
+	public static Permission build(String code, List<AccessType> availableAccessTypes) {
+		return new Permission(code, availableAccessTypes);
+	}
+
+	public static Permission build(String code, String availableAccessType) {
+		List<AccessType> availableAccessTypes = new ArrayList<>();
+		availableAccessTypes.add(AccessType.valueOf(availableAccessType));
+		return new Permission(code, availableAccessTypes);
+	}
+
 	public  List<Permission> toList() {
 		ArrayList<Permission> list = new ArrayList<>();
 		list.add(this);
