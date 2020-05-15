caa74bfc3cdb7a5f058b112d17b8561d0b552b71
Mike Truso <mike@agileorbit.com>
2020-05-15 11:10:49 -0500

    simplify permission, remove unused context call

--- morpheus-tab-plugin/src/main/groovy/com/morpheusdata/tab/TabPlugin.groovy
+++ morpheus-tab-plugin/src/main/groovy/com/morpheusdata/tab/TabPlugin.groovy
@@ -10,11 +10,10 @@ class TabPlugin extends Plugin {
 		CustomTabProvider customTabProvider = new CustomTabProvider(this, morpheusContext)
 		this.pluginProviders.put(customTabProvider.providerCode, customTabProvider)
 		this.setName("Custom Tabs")
-		this.setPermissions([Permission.build('Custom Instance Tab','custom-instance-tab', [Permission.AccessType.none, Permission.AccessType.read, Permission.AccessType.full])])
+		this.setPermissions([Permission.build('Custom Instance Tab','custom-instance-tab', [Permission.AccessType.none, Permission.AccessType.full])])
 	}
 
 	@Override
 	void onDestroy() {
-		morpheusContext.deletePermission('custom-instance-tab').blockingGet()
 	}
 }
