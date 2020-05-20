6cbb06bfa68e99f05788e714323677953c7dc52d
Mike Truso <mike@agileorbit.com>
2020-05-20 16:00:52 -0500

    move command methods to root context, rename methods

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/MorpheusTaskContext.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/MorpheusTaskContext.java
@@ -4,7 +4,6 @@ import com.morpheusdata.model.*;
 import io.reactivex.Single;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 
 public interface MorpheusTaskContext {
@@ -14,7 +13,4 @@ public interface MorpheusTaskContext {
 	Single<TaskConfig> buildRemoteTaskConfig(Map baseConfig, Task task, Collection excludes, Map opts);
 	Single<TaskConfig> buildContainerTaskConfig(Container container, Map baseConfig, Task task, Collection excludes, Map opts);
 	Single<TaskConfig> buildComputeServerTaskConfig(ComputeServer container, Map baseConfig, Task task, Collection excludes, Map opts);
-	Single<TaskResult> executeSudoCommand(String address, Integer port, String username, String password, String command, String publicKey, String privateKey, String passPhrase, Boolean ignoreExitStatus, ComputeServer computeServer, LogLevel logLevel, Boolean doPty, String runAsUser);
-	Single<TaskResult> executeContainerCommands(Container container, List<String> commandList);
-	Single<TaskResult> executeComputeServerCommand(ComputeServer server, String command, Map opts);
 }
