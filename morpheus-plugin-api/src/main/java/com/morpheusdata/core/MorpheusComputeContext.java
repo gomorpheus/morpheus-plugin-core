56eddd9be5647bf4cf42db934767eacd3a161823
Mike Truso <mike@agileorbit.com>
2020-06-01 14:28:03 -0500

    remove unused context method

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/MorpheusComputeContext.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/MorpheusComputeContext.java
@@ -1,7 +1,5 @@
 package com.morpheusdata.core;
 
-import com.morpheusdata.model.AccountIntegration;
-import com.morpheusdata.model.Instance;
 import com.morpheusdata.model.Zone;
 
 import java.util.Date;
@@ -9,5 +7,4 @@ import java.util.Date;
 public interface MorpheusComputeContext {
 
 	void updateZoneStatus(Zone zone, String status, String message, Date syncDate);
-	void requestApproval(Instance instance, String providerCode);
 }
