87e087f329ac517094aa0c387d86770b61ddc8e8
Mike Truso <mike@agileorbit.com>
2020-06-01 11:24:20 -0500

    app or instance approval

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/ApprovalProvider.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/ApprovalProvider.java
@@ -1,5 +1,6 @@
 package com.morpheusdata.core;
 
+import com.morpheusdata.model.App;
 import com.morpheusdata.model.Instance;
 import com.morpheusdata.model.Request;
 import com.morpheusdata.response.RequestResponse;
@@ -8,6 +9,13 @@ import java.util.List;
 import java.util.Map;
 
 public interface ApprovalProvider extends PluginProvider {
-	RequestResponse createApprovalRequest(List<Instance> instances, Request request, Map opts);
+	/**
+	 *
+	 * @param instances List of {@link Instance} or {@link App}
+	 * @param request the Morpheus provision Request
+	 * @param opts provision options
+	 * @return a response object with a success status and references to external approval system
+	 */
+	RequestResponse createApprovalRequest(List instances, Request request, Map opts);
 	List<Request> monitorApproval();
 }
