ed7880ff9d6e9705cfdb5eb7d222e5cbf5eb2fa9
Mike Truso <mike@agileorbit.com>
2020-06-16 13:44:57 -0500

    more documentation

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/InstanceTabProvider.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/InstanceTabProvider.java
@@ -5,7 +5,23 @@ import com.morpheusdata.model.Instance;
 import com.morpheusdata.views.Renderer;
 import com.morpheusdata.views.TemplateResponse;
 
+/**
+ * Provides support for custom UI tabs when viewing Instance details
+ *
+ * @author Mike Truso
+ */
 public interface InstanceTabProvider extends PluginProvider {
+
+	/**
+	 * Default is Handlebars
+	 * @return renderer of specified type
+	 */
 	Renderer<?> getRenderer();
+
+	/**
+	 * Instance details provided to your rendering engine
+	 * @param instance details of an Instance
+	 * @return result of rendering an template
+	 */
 	TemplateResponse renderTemplate(Instance instance);
 }
