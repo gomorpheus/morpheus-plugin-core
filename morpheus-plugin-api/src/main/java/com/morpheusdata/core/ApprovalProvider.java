fa964d26b488b3a257882fe558d5d3f18995d7ab
Mike Truso <mike@agileorbit.com>
2020-07-01 11:03:55 -0500

    get AccountIntegration details on creating an approval request

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/ApprovalProvider.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/ApprovalProvider.java
@@ -16,11 +16,12 @@ public interface ApprovalProvider extends PluginProvider {
 	 *
 	 * @param instances List of {@link Instance} or {@link App}
 	 * @param request the Morpheus provision Request
+	 * @param accountIntegration the integration details. OptionType values are keyed under configMap.cm.plugin
 	 * @param policy the approval policy containing a Map config with values from provided optionTypes
 	 * @param opts provision options
 	 * @return a response object with a success status and references to external approval system
 	 */
-	RequestResponse createApprovalRequest(List instances, Request request, Policy policy, Map opts);
+	RequestResponse createApprovalRequest(List instances, Request request, AccountIntegration accountIntegration, Policy policy, Map opts);
 
 	/**
 	 * Periodically called to check on approval status
@@ -29,8 +30,14 @@ public interface ApprovalProvider extends PluginProvider {
 	List<Request> monitorApproval();
 
 	/**
-	 * Optionally provide custom configuration options when creating a new policy
+	 * Optionally provide custom configuration options when creating a new {@link Policy}
 	 * @return a List of OptionType
 	 */
-	List<OptionType> policyIntegrationOptionTypes();
+	List<OptionType> integrationOptionTypes();
+
+	/**
+	 * Optionally provide custom configuration options when creating a new {@link AccountIntegration}
+	 * @return a List of OptionType
+	 */
+	List<OptionType> policyOptionTypes();
 }
