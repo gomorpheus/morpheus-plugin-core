448f1402ae614637e14b0d2c062e5edbab5f4ae0
Mike Truso <mike@agileorbit.com>
2020-07-07 14:37:22 -0500

    task documentation

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/AbstractTaskService.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/AbstractTaskService.java
@@ -7,29 +7,80 @@ import java.util.Collection;
 import java.util.Map;
 
 /**
- * Provides helper methods to build task configurations
+ * Provides helper methods to build task configurations.
+ * These builder methods can be called from the various {@link ExecutableTaskInterface} execute methods to obtain the
+ * necessary resource details for task execution.
  *
  * @see MorpheusTaskContext
  *
  * @author Mike Truso
  */
 public abstract class AbstractTaskService implements ExecutableTaskInterface {
+
+	/**
+	 * Get configuration details for execution in a local context.
+	 *
+	 * @param baseConfig generally an empty map
+	 * @param task the Morpheus Task to be executed
+	 * @param excludes String property names to exclude
+	 * @param opts options supplied from the execute method
+	 * @return {@link TaskConfig}
+	 */
 	public Single<TaskConfig> buildLocalTaskConfig(Map baseConfig, Task task, Collection excludes, Map opts) {
 		return getContext().getTask().buildLocalTaskConfig(baseConfig, task, excludes, opts);
 	}
 
+	/**
+	 * Get configuration details for execution on an Instance
+	 *
+	 * @param instance the provisioned instance
+	 * @param baseConfig generally an empty map
+	 * @param task the Morpheus Task to be executed
+	 * @param excludes String property names to exclude
+	 * @param opts options supplied from the execute method
+	 * @return {@link TaskConfig}
+	 */
 	public Single<TaskConfig> buildInstanceTaskConfig(Instance instance, Map baseConfig, Task task, Collection excludes, Map opts) {
 		return getContext().getTask().buildInstanceTaskConfig(instance, baseConfig, task, excludes, opts);
 	}
 
+	/**
+	 * Get configuration details for execution in a remote context.
+	 *
+	 * @param baseConfig generally an empty map
+	 * @param task the Morpheus Task to be executed
+	 * @param excludes String property names to exclude
+	 * @param opts options supplied from the execute method
+	 * @return {@link TaskConfig}
+	 */
 	public Single<TaskConfig> buildRemoteTaskConfig(Map baseConfig, Task task, Collection excludes, Map opts) {
 		return getContext().getTask().buildRemoteTaskConfig(baseConfig, task, excludes, opts);
 	}
 
+	/**
+	 * Get configuration details for execution in a container.
+	 *
+	 * @param container the provisioned VM or Container
+	 * @param baseConfig generally an empty map
+	 * @param task the Morpheus Task to be executed
+	 * @param excludes String property names to exclude
+	 * @param opts options supplied from the execute method
+	 * @return {@link TaskConfig}
+	 */
 	public Single<TaskConfig> buildContainerTaskConfig(Container container, Map baseConfig, Task task, Collection excludes, Map opts) {
 		return getContext().getTask().buildContainerTaskConfig(container, baseConfig, task, excludes, opts);
 	}
 
+	/**
+	 * Get configuration details for execution in a remote server.
+	 *
+	 * @param server the provisioned server
+	 * @param baseConfig generally an empty map
+	 * @param task the Morpheus Task to be executed
+	 * @param excludes String property names to exclude
+	 * @param opts options supplied from the execute method
+	 * @return {@link TaskConfig}
+	 */
 	public Single<TaskConfig> buildComputeServerTaskConfig(ComputeServer server, Map baseConfig, Task task, Collection excludes, Map opts) {
 		return getContext().getTask().buildComputeServerTaskConfig(server, baseConfig, task, excludes, opts);
 	}
--- morpheus-plugin-api/src/main/java/com/morpheusdata/model/TaskResult.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/TaskResult.java
@@ -4,16 +4,57 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Result of task execution. These results are displayed in the Morpheus task execution UI
+ *
+ * @author Mike Truso
+ */
 public class TaskResult {
+
+	/**
+	 * whether the task execution was successful or not
+	 */
 	public Boolean success;
+
+	/**
+	 * the input data supplied to the task
+	 */
 	public Object data;
+
+	/**
+	 * optional task output
+	 */
 	public String output;
+
+	/**
+	 * exit status of the process
+	 */
 	public String exitCode;
+
+	/**
+	 * description of the execution result
+	 */
 	public String msg;
+
+	/**
+	 * error message
+	 */
 	public String error;
+
+	/**
+	 * In the event that a Task has subtasks, one or more result objects may be returned
+	 */
 	public List results;
+
+	/**
+	 * custom options derived from the Task OptionTypes
+	 */
 	public Map customOptions;
 
+	/**
+	 *
+	 * @return hash map of TaskResult properties and values
+	 */
 	public Map toMap() {
 		Map<String, Object> taskResultMap = new HashMap<>();
 		taskResultMap.put("success", this.success);
