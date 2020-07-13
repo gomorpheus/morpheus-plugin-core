271459b4441d0bf5804063951c19883d8357468c
Mike Truso <mike@agileorbit.com>
2020-07-13 12:43:34 -0500

    documenting User

--- morpheus-plugin-api/src/main/java/com/morpheusdata/model/User.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/User.java
@@ -2,7 +2,17 @@ package com.morpheusdata.model;
 
 import java.util.Map;
 
+/**
+ * A Morpheus User
+ */
 public class User extends MorpheusModel {
+	/**
+	 * the User's login username
+	 */
     public String username;
-    public Map<String, String> permissions;
+
+	/**
+	 * A Map of the user's permissions where the key is the {@link Permission#code} and the value is the highest {@link com.morpheusdata.model.Permission.AccessType}
+	 */
+	public Map<String, String> permissions;
 }
