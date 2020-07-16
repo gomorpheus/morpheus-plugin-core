c97e6248b7756553a756a429a289f130e1754409
Mike Truso <mike@agileorbit.com>
2020-07-16 16:41:48 -0500

    move instance config to morpheus context

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/MorpheusTaskContext.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/MorpheusTaskContext.java
@@ -9,7 +9,6 @@ import java.util.Map;
 public interface MorpheusTaskContext {
 	Single<Void> disableTask(String code);
 	Single<TaskConfig> buildLocalTaskConfig(Map baseConfig, Task task, Collection excludes, Map opts);
-	Single<TaskConfig> buildInstanceTaskConfig(Instance instance, Map baseConfig, Task task, Collection excludes, Map opts);
 	Single<TaskConfig> buildRemoteTaskConfig(Map baseConfig, Task task, Collection excludes, Map opts);
 	Single<TaskConfig> buildContainerTaskConfig(Container container, Map baseConfig, Task task, Collection excludes, Map opts);
 	Single<TaskConfig> buildComputeServerTaskConfig(ComputeServer container, Map baseConfig, Task task, Collection excludes, Map opts);
