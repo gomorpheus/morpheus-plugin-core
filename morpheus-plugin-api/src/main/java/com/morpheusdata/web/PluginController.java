9b76743c75f835afc3026910cca0fb50dfac8569
Eric Helgeson <erichelgeson@gmail.com>
2020-10-07 15:22:54 -0500

    Javadoc PluginController

--- morpheus-plugin-api/src/main/java/com/morpheusdata/web/PluginController.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/web/PluginController.java
@@ -6,5 +6,9 @@ import java.util.List;
  * Plugin Controllers must implement this interface and define a list of Routes they handle.
  */
 public interface PluginController {
+	/**
+	 * Defines a list of routes the controller can handle.
+	 * @return List of Routes
+	 */
 	public List<Route> getRoutes();
 }
