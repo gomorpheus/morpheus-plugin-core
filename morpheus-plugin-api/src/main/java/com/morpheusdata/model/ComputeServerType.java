2ef7c945fcb86963477edb48899e0db43da3aefb
Mike Truso <mike@agileorbit.com>
2020-10-08 16:08:24 -0500

    initial cloud plugin

--- morpheus-plugin-api/src/main/java/com/morpheusdata/model/ComputeServerType.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/ComputeServerType.java
@@ -9,6 +9,13 @@ package com.morpheusdata.model;
 public class ComputeServerType  extends MorpheusModel {
 
 	private String name;
+	private String code;
+	private String description;
+	private String computeService;
+	private Boolean vmHypervisor = false; //runs vms, ex: esxi hypervisor
+	private Boolean containerHypervisor = false; //runs docker
+	private Boolean bareMetalHost = false; //bare metal
+	private Boolean guestVm = false; //is a vm
 
 
 	public String getName() {
@@ -19,6 +26,63 @@ public class ComputeServerType  extends MorpheusModel {
 		this.name = name;
 		markDirty("name", name);
 	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getComputeService() {
+		return computeService;
+	}
+
+	public void setComputeService(String computeService) {
+		this.computeService = computeService;
+	}
+
+	public Boolean getVmHypervisor() {
+		return vmHypervisor;
+	}
+
+	public void setVmHypervisor(Boolean vmHypervisor) {
+		this.vmHypervisor = vmHypervisor;
+	}
+
+	public Boolean getContainerHypervisor() {
+		return containerHypervisor;
+	}
+
+	public void setContainerHypervisor(Boolean containerHypervisor) {
+		this.containerHypervisor = containerHypervisor;
+	}
+
+	public Boolean getBareMetalHost() {
+		return bareMetalHost;
+	}
+
+	public void setBareMetalHost(Boolean bareMetalHost) {
+		this.bareMetalHost = bareMetalHost;
+	}
+
+	public Boolean getGuestVm() {
+		return guestVm;
+	}
+
+	public void setGuestVm(Boolean guestVm) {
+		this.guestVm = guestVm;
+	}
+
 
 
 }
