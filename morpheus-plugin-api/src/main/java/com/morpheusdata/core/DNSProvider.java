5c2083446f309b0a847a12c3ec82e0a243ba5115
davydotcom <davydotcom@gmail.com>
2020-10-13 09:02:33 -0400

    adding javadoc for IPAM provider and fixing definition duplication into DNS Provider

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/DNSProvider.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/DNSProvider.java
@@ -1,9 +1,6 @@
 package com.morpheusdata.core;
 
-import com.morpheusdata.model.AccountIntegration;
-import com.morpheusdata.model.ComputeServer;
-import com.morpheusdata.model.Container;
-import com.morpheusdata.model.Workload;
+import com.morpheusdata.model.*;
 import com.morpheusdata.response.ServiceResponse;
 
 import java.util.Map;
@@ -19,4 +16,15 @@ public interface DNSProvider extends PluginProvider {
 	ServiceResponse provisionServer(AccountIntegration integration, ComputeServer server, Map opts);
 	ServiceResponse removeServer(AccountIntegration integration, ComputeServer server, Map opts);
 	ServiceResponse removeContainer(AccountIntegration integration, Container container, Map opts);
+
+	/**
+	 * Creates a manually allocated DNS Record of the specified record type on the passed {@link NetworkDomainRecord} object.
+	 * This is typically called outside of automation and is a manual method for administration purposes.
+	 * @param integration The DNS Integration record which contains things like connectivity info to the DNS Provider
+	 * @param record The domain record that is being requested for creation. All the metadata needed to create teh record
+	 *               should exist here.
+	 * @param opts any additional options that may be used in the future to configure behavior. Currently unused
+	 * @return a ServiceResponse with the success/error state of the create operation as well as the modified record.
+	 */
+	ServiceResponse createRecord(AccountIntegration integration, NetworkDomainRecord record, Map opts);
 }
