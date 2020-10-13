8326432abe1d1814c60a25c5a0d95253644cb34d
davydotcom <davydotcom@gmail.com>
2020-10-13 09:02:33 -0400

    adding javadoc for IPAM provider and fixing definition duplication into DNS Provider

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/IPAMProvider.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/IPAMProvider.java
@@ -23,13 +23,27 @@ public interface IPAMProvider extends PluginProvider {
 	 * If the error is a generic authentication error or unknown error, a standard message can also be sent back in the response.
 	 *
 	 * @param poolServer The Integration Object contains all the saved information regarding configuration of the IPAM Provider.
-	 * @param opts Pagination options
+	 * @param opts any custom payload submission options may exist here
 	 * @return A response is returned depending on if the inputs are valid or not.
 	 */
 	public ServiceResponse verifyNetworkPoolServer(NetworkPoolServer poolServer, Map opts);
 
+	/**
+	 * Called during creation of a {@link NetworkPoolServer} operation. This allows for any custom operations that need
+	 * to be performed outside of the standard operations.
+	 * @param poolServer The Integration Object contains all the saved information regarding configuration of the IPAM Provider.
+	 * @param opts any custom payload submission options may exist here
+	 * @return A response is returned depending on if the operation was a success or not.
+	 */
 	public ServiceResponse createNetworkPoolServer(NetworkPoolServer poolServer, Map opts);
 
+	/**
+	 * Called during update of an existing {@link NetworkPoolServer}. This allows for any custom operations that need
+	 * to be performed outside of the standard operations.
+	 * @param poolServer The Integration Object contains all the saved information regarding configuration of the IPAM Provider.
+	 * @param opts any custom payload submission options may exist here
+	 * @return A response is returned depending on if the operation was a success or not.
+	 */
 	public ServiceResponse updateNetworkPoolServer(NetworkPoolServer poolServer, Map opts);
 
 	/**
@@ -55,27 +69,76 @@ public interface IPAMProvider extends PluginProvider {
 
 	/*
 	 * Target endpoint used to allocate an IP Address during provisioning of Instances
+	 * @param networkPoolServer The Integration Object contains all the saved information regarding configuration of the IPAM Provider.
+	 * @param networkPool the NetworkPool currently being operated on that contains the ip address for reservation
+	 * @param network The Network reference object on the cloud that the ip address is reserved for
+	 * @param assignedType the reference object type this ip address is being assigned to. Typically this relates to a server or container, in the future it could expand to a VIP
+	 * @param assignedId the reference object id based on the object type the ip address is being assigned to
+	 * @param subAssignedId the sub object attached to the reference object this ip address is being reserved for. Typically this is the network interface id on the server
+	 * @param assignedHostname the hostname of the server/interface the ip is being allocated for. Typically this would be assigned on the host record and also used to create a PTR or A record automatically
+	 * @param opts list of additional options that can be passed for reservation. for example, if a user wants a specific ip address. this exists as opts.ipAddress
 	 */
 	ServiceResponse leasePoolAddress(NetworkPoolServer networkPoolServer, NetworkPool networkPool, Network network, String assignedType, Long assignedId, Long subAssignedId, String assignedHostname, Map opts);
 
 	/*
 	 * Called during provisioning to setup a DHCP Lease address by mac address. This can be used in some scenarios in the event the environment supports DHCP Reservations instead of strictly static
+	 * @param networkPoolServer The Integration Object contains all the saved information regarding configuration of the IPAM Provider.
+	 * @param networkPool the NetworkPool currently being operated on that contains the ip address for reservation
+	 * @param network The Network reference object on the cloud that the ip address is reserved for
+	 * @param assignedType the reference object type this ip address is being assigned to. Typically this relates to a server or container, in the future it could expand to a VIP
+	 * @param assignedId the reference object id based on the object type the ip address is being assigned to
+	 * @param subAssignedId the sub object attached to the reference object this ip address is being reserved for. Typically this is the network interface id on the server
+	 * @param assignedHostname the hostname of the server/interface the ip is being allocated for. Typically this would be assigned on the host record and also used to create a PTR or A record automatically
+	 * @param opts list of additional options that can be passed for reservation. for example, if a user wants a specific ip address. this exists as opts.ipAddress
 	 */
 	ServiceResponse reservePoolAddress(NetworkPoolServer networkPoolServer, NetworkPool networkPool, Network network, String assignedType, Long assignedId, Long subAssignedId, String assignedHostname, Map opts);
 
 	/*
 	 * Called during instance teardown to release an IP Address reservation.
+	 * @param networkPoolServer The Integration Object contains all the saved information regarding configuration of the IPAM Provider.
+	 * @param networkPool the NetworkPool currently being operated on that contains the ip address for release
+	 * @param network The Network reference object on the cloud that the ip address is being released from
+	 * @param ipAddress the IP Addresse entry record that needs to be released
+	 * @param opts additional options that can sometimes be passed. TODO: Fill out available option keys
 	 */
 	ServiceResponse returnPoolAddress(NetworkPoolServer networkPoolServer, NetworkPool networkPool, Network network, NetworkPoolIp ipAddress, Map opts);
 
 
 	ServiceResponse initializeNetworkPoolServer(NetworkPoolServer poolServer, Map opts);
-	ServiceResponse createRecord(AccountIntegration integration, NetworkDomainRecord record, Map opts);
+
+	/**
+	 * Creates a Host record on the target {@link NetworkPool} within the {@link NetworkPoolServer} integration.
+	 * Typically called outside of automation and is used for administration purposes.
+	 * @param poolServer The Integration Object contains all the saved information regarding configuration of the IPAM Provider.
+	 * @param networkPool the NetworkPool currently being operated on.
+	 * @param networkPoolIp The ip address and metadata related to it for allocation. It is important to create functionality such that
+	 *                      if the ip address property is blank on this record, auto allocation should be performed and this object along with the new
+	 *                      ip address be returned in the {@link ServiceResponse}
+	 * @param domain The domain with which we optionally want to create an A/PTR record for during this creation process.
+	 * @param createARecord configures whether or not the A record is automatically created
+	 * @param createPtrRecord configures whether or not the PTR record is automatically created
+	 * @return a ServiceResponse containing the success state of the create host record operation
+	 */
 	ServiceResponse createHostRecord(NetworkPoolServer poolServer, NetworkPool  networkPool, NetworkPoolIp networkPoolIp, NetworkDomain domain, Boolean createARecord, Boolean createPtrRecord); // createHostRecord
+
+	/**
+	 * Updates a Host record on the target {@link NetworkPool} if supported by the Provider. If not supported, send the appropriate
+	 * {@link ServiceResponse} such that the user is properly informed of the unavailable operation.
+	 * @param poolServer The Integration Object contains all the saved information regarding configuration of the IPAM Provider.
+	 * @param networkPool the NetworkPool currently being operated on.
+	 * @param networkPoolIp the changes to the network pool ip address that would like to be made. Most often this is just the host record name.
+	 * @return a ServiceResponse containing the success state of the update host record operation
+	 */
 	ServiceResponse updateHostRecord(NetworkPoolServer poolServer, NetworkPool networkPool, NetworkPoolIp networkPoolIp);
+
+	/**
+	 * Deletes a host record on the target {@link NetworkPool}. This is used for cleanup or releasing of an ip address on
+	 * the IPAM Provider.
+	 * @param networkPool the NetworkPool currently being operated on.
+	 * @param poolIp the record that is being deleted.
+	 * @param deleteAssociatedRecords determines if associated records like A/PTR records
+	 * @return a ServiceResponse containing the success state of the delete operation
+	 */
 	ServiceResponse deleteHostRecord(NetworkPool networkPool, NetworkPoolIp poolIp, Boolean deleteAssociatedRecords);
-	ServiceResponse provisionWorkload(AccountIntegration integration, Workload workload, Map opts);
-	ServiceResponse provisionServer(AccountIntegration integration, ComputeServer server, Map opts);
-	ServiceResponse removeServer(AccountIntegration integration, ComputeServer server, Map opts);
-	ServiceResponse removeContainer(AccountIntegration integration, Container container, Map opts);
+	
 }
