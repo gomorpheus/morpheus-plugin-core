3edc4408c8a4240175f85e87b3114341e515ffb2
Mike Truso <mike@agileorbit.com>
2020-12-16 11:35:54 -0600

    stopWorkload

--- samples/morpheus-cloud-plugin/src/main/groovy/com/morpheusdata/cloud/DigitalOceanProvisionProvider.groovy
+++ samples/morpheus-cloud-plugin/src/main/groovy/com/morpheusdata/cloud/DigitalOceanProvisionProvider.groovy
@@ -8,14 +8,10 @@ import com.morpheusdata.model.OptionType
 import com.morpheusdata.model.Workload
 import com.morpheusdata.response.ServiceResponse
 import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
 import org.apache.http.client.methods.HttpDelete
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
-import org.apache.http.impl.client.CloseableHttpClient
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.util.EntityUtils
 
 class DigitalOceanProvisionProvider implements ProvisioningProvider {
 	Plugin plugin
@@ -68,16 +64,13 @@ class DigitalOceanProvisionProvider implements ProvisioningProvider {
 	ServiceResponse runWorkload(Workload workload, Map opts) {
 		println "DO Provision Provider: runWorkload"
 		HttpPost http = new HttpPost("${DIGITAL_OCEAN_ENDPOINT}/v2/droplets")
-		http.addHeader("Authorization", "Bearer ${workload.server.cloud.configMap.doApiKey}")
-		http.addHeader('Content-Type', 'application/json')
-		http.addHeader('Accept', 'application/json')
 		def body = [
 				'name'              : opts.name,
 				'region'            : opts.datacenterName,
 				'size'              : opts.sizeRef,
 				'image'             : opts.imageRef,
 				'ssh_keys'          : opts.sshKeys,
-				'backups'           : "${ opts.doBackups == true }",
+				'backups'           : "${opts.doBackups == true}",
 				'ipv6'              : opts.ipv6 == "true",
 				'user_data'         : opts.userData,
 				'private_networking': opts.privateNetworking == "true"
@@ -85,25 +78,37 @@ class DigitalOceanProvisionProvider implements ProvisioningProvider {
 		println "post body: $body"
 		http.entity = new StringEntity(JsonOutput.toJson(body))
 
-		CloseableHttpClient client = HttpClients.createDefault()
-		def resp = client.execute(http)
-		String responseContent = EntityUtils.toString(resp.entity)
-		JsonSlurper slurper = new JsonSlurper()
-		def json = slurper.parseText(responseContent)
-		println json
+		def respMap = apiService.makeApiCall(http, workload.server.cloud.configMap.doApiKey)
 
-		if (resp.statusLine.statusCode == 202) {
+		if (respMap.resp.statusLine.statusCode == 202) {
 			println "Droplet Created"
-			return new ServiceResponse(success: true, data: json.droplet)
+			return new ServiceResponse(success: true, data: respMap.json.droplet)
 		} else {
-			println "Failed to create droplet: $responseContent"
-			return new ServiceResponse(success: false, msg: resp?.statusLine?.statusCode, content: responseContent, error: responseContent)
+			println "Failed to create droplet: $respMap.resp"
+			return new ServiceResponse(success: false, msg: respMap?.resp?.statusLine?.statusCode, content: respMap.resp, error: respMap.resp)
 		}
 	}
 
 	@Override
 	ServiceResponse stopWorkload(Workload workload) {
-		return null
+		String dropletId = workload.server.externalId
+		String apiKey = workload.server.cloud.configMap.doApiKey
+		println "stop server: ${dropletId}"
+		if (!dropletId) {
+			println "no Droplet ID provided"
+			return new ServiceResponse(success: true, msg: 'No Droplet ID provided')
+		}
+
+		HttpPost http = new HttpPost("${DIGITAL_OCEAN_ENDPOINT}/v2/droplets/${dropletId}/actions")
+		def body = ['type': 'shutdown']
+		http.entity = new StringEntity(JsonOutput.toJson(body))
+		Map respMap = apiService.makeApiCall(http, apiKey)
+
+		if (respMap?.resp?.statusLine?.statusCode == 201) {
+			return new ServiceResponse(success: true, data: respMap.json.action)
+		} else {
+			powerOffServer(apiKey, dropletId)
+		}
 	}
 
 	@Override
@@ -159,14 +164,14 @@ class DigitalOceanProvisionProvider implements ProvisioningProvider {
 		ServiceResponse resp = new ServiceResponse(success: false)
 		Boolean pending = true
 		Integer attempts = 0
-		while(pending) {
+		while (pending) {
 			println "attempt $attempts"
 			sleep(1000l * 20l)
 			resp = serverStatus(server)
 			if (resp.success || resp.msg == 'failed') {
 				pending = false
 			}
-			attempts ++
+			attempts++
 			if (attempts > 15) {
 				pending = false
 			}
@@ -187,5 +192,19 @@ class DigitalOceanProvisionProvider implements ProvisioningProvider {
 		}
 		resp.content = respMap.json
 		resp.msg = status
+	}
+
+	ServiceResponse powerOffServer(String apiKey, String dropletId) {
+		println "power off server"
+		HttpPost http = new HttpPost("${DIGITAL_OCEAN_ENDPOINT}/v2/droplets/${dropletId}/actions")
+		def body = ['type': 'power_off']
+		http.entity = new StringEntity(JsonOutput.toJson(body))
+		Map respMap = apiService.makeApiCall(http, apiKey)
+
+		if (respMap?.resp?.statusLine?.statusCode == 201) {
+			return new ServiceResponse(success: true, data: respMap.json.action)
+		} else {
+			return new ServiceResponse(success: false, content: respMap?.json, msg: respMap?.resp?.statusLine?.statusCode, error: respMap?.json)
+		}
 	}
 }
