f22f797c90e48a47e76be2d11749202926bc7860
Mike Truso <mike@agileorbit.com>
2021-02-22 09:50:25 -0600

    compute context calls

--- morpheus-test-provider/src/main/groovy/com/morpheusdata/MorpheusComputeContextImpl.groovy
+++ morpheus-test-provider/src/main/groovy/com/morpheusdata/MorpheusComputeContextImpl.groovy
@@ -4,6 +4,8 @@ import com.morpheusdata.core.MorpheusComputeContext
 import com.morpheusdata.model.Account
 import com.morpheusdata.model.Cloud
 import com.morpheusdata.model.KeyPair
+import com.morpheusdata.model.ServicePlan
+import com.morpheusdata.model.VirtualImage
 import io.reactivex.Single
 
 class MorpheusComputeContextImpl implements MorpheusComputeContext {
@@ -20,6 +22,16 @@ class MorpheusComputeContextImpl implements MorpheusComputeContext {
 
 	@Override
 	Single<Void> updateKeyPair(KeyPair keyPair, Cloud cloud) {
+		return null
+	}
+
+	@Override
+	Single<Void> cacheImages(List<VirtualImage> virtualImages, Cloud cloud) {
+		return null
+	}
+
+	@Override
+	Single<Void> cachePlans(List<ServicePlan> servicePlans) {
 		return null
 	}
 }
