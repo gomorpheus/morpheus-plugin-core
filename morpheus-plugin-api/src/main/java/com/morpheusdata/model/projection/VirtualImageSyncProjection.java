7a4580e637d075a2c8f7f35945099aee577a06ae
Mike Truso <mike@agileorbit.com>
2021-03-11 17:01:03 -0600

    virtual image javadocs

--- morpheus-plugin-api/src/main/java/com/morpheusdata/model/projection/VirtualImageSyncProjection.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/projection/VirtualImageSyncProjection.java
@@ -1,5 +1,11 @@
 package com.morpheusdata.model.projection;
 
+/**
+ * Provides a subset of properties from the {@link com.morpheusdata.model.VirtualImage} object for doing a sync match
+ * comparison with less bandwidth usage and memory footprint. This is a DTO Projection object
+ * @see com.morpheusdata.core.MorpheusVirtualImageContext
+ * @author Mike Truso
+ */
 public class VirtualImageSyncProjection {
 	public Long id;
 	public String externalId;
