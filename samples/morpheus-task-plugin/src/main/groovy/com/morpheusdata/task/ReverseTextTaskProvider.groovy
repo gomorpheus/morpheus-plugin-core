f1f3c601eae2dc6ebaeca1f9617f9795dffd237e
David Estes <davydotcom@gmail.com>
2022-04-13 08:58:43 -0400

    adding has results

--- samples/morpheus-task-plugin/src/main/groovy/com/morpheusdata/task/ReverseTextTaskProvider.groovy
+++ samples/morpheus-task-plugin/src/main/groovy/com/morpheusdata/task/ReverseTextTaskProvider.groovy
@@ -77,6 +77,11 @@ class ReverseTextTaskProvider implements TaskProvider {
 		return true
 	}
 
+	@Override
+	Boolean hasResults() {
+		return true
+	}
+
 	/**
 	 * Builds an OptionType to take some text
 	 * @return list of OptionType
