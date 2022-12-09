fa466cb75e233f59eda3da5a0b4fc252ddeb4a9a
Dustin Deyoung <ddeyoung@morpheusdata.com>
2022-12-09 14:34:55 -0500

    More backup plugin documentation.

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/backup/util/BackupResultUtility.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/backup/util/BackupResultUtility.java
@@ -4,6 +4,10 @@ import com.morpheusdata.core.util.KeyUtility;
 
 public class BackupResultUtility {
 
+	/**
+	 * Generate a random UUID to be used as the result set id on a {@link com.morpheusdata.model.BackupResult}
+	 * @return String a 10 character UUID
+	 */
 	public static String generateBackupResultSetId() {
 		return KeyUtility.generateDateKey(10);
 	}
