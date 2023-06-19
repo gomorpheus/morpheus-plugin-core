fc5e3ab5425167e5530f415b38d55a350c179036
Alex Clement <aclement@morpheusdata.com>
2023-06-19 11:45:33 -0600

    Add serverGroup as a field on server

--- morpheus-plugin-api/src/main/java/com/morpheusdata/model/ComputeServer.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/ComputeServer.java
@@ -34,6 +34,8 @@ public class ComputeServer extends ComputeServerIdentityProjection {
 	protected ComputeTypeSet typeSet;
 	@JsonSerialize(using= ModelAsIdOnlySerializer.class)
 	protected ComputeTypeLayout layout;
+	@JsonSerialize(using= ModelAsIdOnlySerializer.class)
+	protected ComputeServerGroup serverGroup;
 	protected String internalName;
 	protected String status = "provisioning";
 	protected Long provisionSiteId;
@@ -842,6 +844,15 @@ public class ComputeServer extends ComputeServerIdentityProjection {
 	public void setDiscovered(Boolean discovered) {
 		this.discovered = discovered;
 		markDirty("discovered", discovered);
+	}
+
+	public ComputeServerGroup getServerGroup() {
+		return serverGroup;
+	}
+
+	public void setServerGroup(ComputeServerGroup serverGroup) {
+		this.serverGroup = serverGroup;
+		markDirty("serverGroup", serverGroup);
 	}
 
 	public void setMetadata(List<MetadataTag> metadata) {
