b8c20bace0b27db25cc17707d39c9a035f548e01
Dustin Deyoung <ddeyoung@morpheusdata.com>
2023-06-21 21:05:28 -0400

    Register account integration services.

---
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/AccountIntegrationObjectRef.java
@@ -0,0 +1,55 @@
+package com.morpheusdata.model;
+
+public class AccountIntegrationObjectRef  extends MorpheusModel {
+
+	protected Long refId;
+	protected String refType;
+	protected String category;
+	protected String config;
+	protected String displayName;
+
+	public Long getRefId() {
+		return refId;
+	}
+
+	public void setRefId(Long refId) {
+		this.refId = refId;
+		markDirty("refId", refId, this.refId);
+	}
+
+	public String getRefType() {
+		return refType;
+	}
+
+	public void setRefType(String refType) {
+		this.refType = refType;
+		markDirty("refType", refType, this.refType);
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+		markDirty("category", category, this.category);
+	}
+
+	public String getConfig() {
+		return config;
+	}
+
+	public void setConfig(String config) {
+		this.config = config;
+		markDirty("config", config, this.config);
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+		markDirty("displayName", displayName, this.displayName);
+	}
+}
