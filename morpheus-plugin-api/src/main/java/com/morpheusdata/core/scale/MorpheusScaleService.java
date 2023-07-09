ac4113075b4a521b705f90884ce6464a56b8f08c
Jordon Saardchit <jordon@saardchit.com>
2023-07-09 15:20:27 -0700

    cleanup of dupe services

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/scale/MorpheusScaleService.java
+++
@@ -1,8 +0,0 @@
-package com.morpheusdata.core.scale;
-
-/**
- * Morpheus context as it relates to scale group operations. Used to retrieve and query various entities related to
- * scale groups
- */
-public interface MorpheusScaleService {
-}
