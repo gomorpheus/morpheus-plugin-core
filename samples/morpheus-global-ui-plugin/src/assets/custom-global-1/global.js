44c8a18309770d65f79255d67eaf7623aab0b3df
Dustin Deyoung <ddeyoung@morpheusdata.com>
2023-08-10 19:50:39 -0400

    Global UI Plugin sample tweaks.

--- samples/morpheus-global-ui-plugin/src/assets/custom-global-1/global.js
+++ samples/morpheus-global-ui-plugin/src/assets/custom-global-1/global.js
@@ -1,1 +1,7 @@
-console.log("Script loaded from Global UI Plugin inside folder");
+alert("hello from the Global UI Plugin");
+
+$(function() {
+	if($('body').data('page') === 'instances/index') {
+		alert("Hello from the instances page")
+	}
+});
