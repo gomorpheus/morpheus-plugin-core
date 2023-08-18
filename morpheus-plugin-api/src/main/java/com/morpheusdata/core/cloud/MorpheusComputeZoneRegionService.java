de00c84d3b98320ec456dcf06d4d3d82f52d4022
Dan DeVilbiss <ddevilbiss@bcap.com>
2023-08-18 16:11:00 -0700

    compute zone region inherits morpheus data service

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/cloud/MorpheusComputeZoneRegionService.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/cloud/MorpheusComputeZoneRegionService.java
@@ -1,15 +1,13 @@
 package com.morpheusdata.core.cloud;
 
 
-import com.morpheusdata.model.ComputeZonePool;
+import com.morpheusdata.core.MorpheusDataService;
 import com.morpheusdata.model.ComputeZoneRegion;
-import com.morpheusdata.model.projection.ComputeZonePoolIdentityProjection;
 import com.morpheusdata.model.projection.ComputeZoneRegionIdentityProjection;
 import io.reactivex.Observable;
 import io.reactivex.Single;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -19,7 +17,7 @@ import java.util.Optional;
  * @author David Estes
  * @since 0.14.0
  */
-public interface MorpheusComputeZoneRegionService {
+public interface MorpheusComputeZoneRegionService extends MorpheusDataService<ComputeZoneRegion> {
 	/**
 	 * Get a list of {@link ComputeZoneRegion} projections based on Cloud id
 	 *
@@ -36,29 +34,5 @@ public interface MorpheusComputeZoneRegionService {
 	 */
 	Observable<ComputeZoneRegion> listById(Collection<Long> ids);
 
-	/**
-	 * Save updates to existing Regions (ComputeZoneRegion)
-	 *
-	 * @param regions updated Region records
-	 * @return success
-	 */
-	Single<Boolean> save(List<ComputeZoneRegion> regions);
-
 	Single<Optional<ComputeZoneRegion>> findByCloudAndRegionCode(Long cloudId, String regionCode);
-
-	/**
-	 * Create new ComputeZoneRegion in Morpheus
-	 *
-	 * @param regions new ComputeZoneRegion to persist
-	 * @return success
-	 */
-	Single<Boolean> create(List<ComputeZoneRegion> regions);
-
-	/**
-	 * Remove persisted ComputeZoneRegion from Morpheus
-	 *
-	 * @param regions Regions to Delete
-	 * @return success
-	 */
-	Single<Boolean> remove(List<ComputeZoneRegionIdentityProjection> regions);
 }
