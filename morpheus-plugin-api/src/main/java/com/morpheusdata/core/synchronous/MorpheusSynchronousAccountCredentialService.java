b061e492acf8b62620e3a83710f4694b03ff9369
Dustin Deyoung <ddeyoung@morpheusdata.com>
2023-09-28 16:27:22 -0400

    Account credential synchronous methods.

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/MorpheusSynchronousAccountCredentialService.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/MorpheusSynchronousAccountCredentialService.java
@@ -3,6 +3,29 @@ package com.morpheusdata.core.synchronous;
 import com.morpheusdata.core.MorpheusSynchronousIdentityService;
 import com.morpheusdata.core.MorpheusSynchronousDataService;
 import com.morpheusdata.model.AccountCredential;
+import com.morpheusdata.model.AccountIntegration;
+import com.morpheusdata.model.BackupProvider;
+import com.morpheusdata.model.Cloud;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+
+import java.util.Map;
 
 public interface MorpheusSynchronousAccountCredentialService extends MorpheusSynchronousDataService<AccountCredential, AccountCredential> {
+
+
+	/**
+	 * A utility method to loads credential data config from the args of an input form.
+	 * May be used in OptionSourceServices to handle when a user selects an AccountCredential
+	 * @param credentialConfig Pass through of args.credential elements from form payload
+	 * @param refConfig The base config to fall back on.. typically cloud.configMap
+	 * @return Observable
+	 */
+	Map loadCredentialConfig(Map credentialConfig, Map refConfig);
+
+	AccountCredential loadCredentials(Cloud cloud);
+
+	AccountCredential loadCredentials(AccountIntegration accountIntegration);
+
+	AccountCredential loadCredentials(BackupProvider backupProvider);
 }
