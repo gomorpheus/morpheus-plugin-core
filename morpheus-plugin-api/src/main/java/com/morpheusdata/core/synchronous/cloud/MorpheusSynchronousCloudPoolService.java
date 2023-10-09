4d86aefc2b390c754e04af01ef5848d38d91d6b6
Dustin Deyoung <ddeyoung@morpheusdata.com>
2023-10-09 14:09:20 -0400

    Add pool method to get pool from pool id string.

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/cloud/MorpheusSynchronousCloudPoolService.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/cloud/MorpheusSynchronousCloudPoolService.java
@@ -4,6 +4,18 @@ import com.morpheusdata.core.MorpheusSynchronousDataService;
 import com.morpheusdata.core.MorpheusSynchronousIdentityService;
 import com.morpheusdata.model.CloudPool;
 import com.morpheusdata.model.projection.CloudPoolIdentity;
+import io.reactivex.Maybe;
 
 public interface MorpheusSynchronousCloudPoolService extends MorpheusSynchronousDataService<CloudPool,CloudPoolIdentity>, MorpheusSynchronousIdentityService<CloudPoolIdentity> {
+
+	/**
+	 * Returns a pool from a pool ID string (usually starts with pool- or poolGroup-) obtained from user inputs. In the case of a pool group ID a
+	 * pool will be selected based on the group's selection mode
+	 * @param poolId
+	 * @param accountId
+	 * @param siteId
+	 * @param zoneId
+	 * @return a cloud pool or null
+	 */
+	CloudPool get(String poolId, Long accountId, Long siteId, Long zoneId);
 }
