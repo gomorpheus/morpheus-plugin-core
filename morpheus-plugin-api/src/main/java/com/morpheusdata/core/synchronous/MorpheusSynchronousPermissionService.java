6ab136b72bfa905dab9cef82a91267f6bb6d48b6
Dustin Deyoung <ddeyoung@morpheusdata.com>
2024-02-14 11:06:04 -0500

    Add missing synchronous permission method.

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/MorpheusSynchronousPermissionService.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/MorpheusSynchronousPermissionService.java
@@ -3,6 +3,18 @@ package com.morpheusdata.core.synchronous;
 import com.morpheusdata.core.MorpheusSynchronousIdentityService;
 import com.morpheusdata.core.MorpheusSynchronousDataService;
 import com.morpheusdata.model.Permission;
+import io.reactivex.rxjava3.core.Observable;
 
 public interface MorpheusSynchronousPermissionService extends MorpheusSynchronousDataService<Permission, Permission> {
+
+	/**
+	 * Get a list of ids for which the given accountId and siteId have access to (optionally scoped to siteId)
+	 * @param accountId the account ID to scope the search
+	 * @param resourceType the ResourceType
+	 * @param siteId (Optional)
+	 * @param planId (Optional)
+	 * @return list of ids
+	 */
+	Observable<Long> listAccessibleResources(Long accountId, Permission.ResourceType resourceType, Long siteId, Long planId);
+
 }
