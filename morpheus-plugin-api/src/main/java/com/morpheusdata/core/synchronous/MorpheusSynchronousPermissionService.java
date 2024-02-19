486818002f872ecd349455e0fbbf8e8032aa841b
Dustin Deyoung <ddeyoung@morpheusdata.com>
2024-02-19 10:27:55 -0500

    Missing method in synchronous permission service.

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/MorpheusSynchronousPermissionService.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/MorpheusSynchronousPermissionService.java
@@ -3,7 +3,7 @@ package com.morpheusdata.core.synchronous;
 import com.morpheusdata.core.MorpheusSynchronousIdentityService;
 import com.morpheusdata.core.MorpheusSynchronousDataService;
 import com.morpheusdata.model.Permission;
-import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.Observable;
 
 public interface MorpheusSynchronousPermissionService extends MorpheusSynchronousDataService<Permission, Permission> {
 
