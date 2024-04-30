da2a179695446b0cdf7e80eee235bd2eac04e71c
Alex Clement <MrClement@users.noreply.github.com>
2024-04-30 11:15:44 -0600

    Add sync getCloudFileStreamUrl

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/MorpheusSynchronousVirtualImageService.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/MorpheusSynchronousVirtualImageService.java
@@ -1,9 +1,23 @@
 package com.morpheusdata.core.synchronous;
 
+import com.bertramlabs.plugins.karman.CloudFile;
 import com.morpheusdata.core.MorpheusSynchronousIdentityService;
 import com.morpheusdata.core.MorpheusSynchronousDataService;
+import com.morpheusdata.model.Cloud;
+import com.morpheusdata.model.User;
 import com.morpheusdata.model.VirtualImage;
 import com.morpheusdata.model.projection.VirtualImageIdentityProjection;
 
 public interface MorpheusSynchronousVirtualImageService extends MorpheusSynchronousDataService<VirtualImage, VirtualImageIdentityProjection>, MorpheusSynchronousIdentityService<VirtualImage> {
+
+	/**
+	 * Get a one-off url for an image to upload to it to a cloud
+	 * @since 0.15.13
+	 * @param virtualImage the image
+	 * @param cloudFile the specific file
+	 * @param createdBy the user associated with the workload or server
+	 * @param cloud the Cloud instance
+	 * @return the url of the image file
+	 */
+	String getCloudFileStreamUrl(VirtualImage virtualImage, CloudFile cloudFile, User createdBy, Cloud cloud);
 }
