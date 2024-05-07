86542d8eee970a6a05efd494425ee53c998fa020
Dustin Deyoung <ddeyoung@morpheusdata.com>
2024-05-07 14:44:58 -0400

    Synchronous services for backup storage.

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/backup/MorpheusSynchronousBackupService.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/backup/MorpheusSynchronousBackupService.java
@@ -1,11 +1,14 @@
 package com.morpheusdata.core.synchronous.backup;
 
+import com.bertramlabs.plugins.karman.StorageProvider;
 import com.morpheusdata.core.MorpheusSynchronousIdentityService;
 import com.morpheusdata.core.MorpheusSynchronousDataService;
 import com.morpheusdata.core.backup.*;
 import com.morpheusdata.model.*;
 import com.morpheusdata.model.BackupProvider;
+import com.morpheusdata.model.projection.AccountIdentity;
 import com.morpheusdata.model.projection.BackupIdentityProjection;
+import io.reactivex.rxjava3.core.Single;
 
 public interface MorpheusSynchronousBackupService extends MorpheusSynchronousDataService<Backup, BackupIdentityProjection>, MorpheusSynchronousIdentityService<BackupIdentityProjection> {
 
@@ -44,5 +47,45 @@ public interface MorpheusSynchronousBackupService extends MorpheusSynchronousDat
 	 * @return An instance of the MorpheusReplicationContext to be used for calls by various backup providers
 	 */
 	MorpheusSynchronousReplicationService getReplication();
+
+	/**
+	 * Returns the {@link StorageBucket} associated with a {@link Backup} object. If the backup does not have an associated
+	 * storage bucket, the default backup storage bucket will be returned.
+	 * @param accountIdentity the {@link AccountIdentity} object to use for the storage bucket lookup
+	 * @param backupId the ID of the {@link Backup} object to use for the storage bucket lookup
+	 * @return the Single Observable containing the {@link StorageBucket} object for subscription
+	 */
+	StorageBucket getBackupStorageBucket(AccountIdentity accountIdentity, Long backupId);
+
+	/**
+	 * Returns the {@link StorageBucket} associated with a {@link Backup} object. If the backup does not have an associated
+	 * storage bucket, the default backup storage bucket will be returned.
+	 * @param accountIdentity the {@link AccountIdentity} object to use for the storage bucket lookup
+	 * @param backupId the ID of the {@link Backup} object to use for the storage bucket lookup
+	 * @param storageProviderId the ID of the {@link StorageProvider} object to use for the storage bucket lookup
+	 * @return the Single Observable containing the {@link StorageBucket} object for subscription
+	 */
+	StorageBucket getBackupStorageBucket(AccountIdentity accountIdentity, Long backupId, Long storageProviderId);
+
+	/**
+	 * Returns the {@link StorageProvider} for the default backup storage bucket.
+	 * @return the Single Observable containing the {@link StorageProvider} object for subscription
+	 */
+	StorageProvider getBackupStorageProvider();
+
+	/**
+	 * Returns the {@link StorageProvider} for a specific storage bucket.
+	 * @param storageBucketId the ID of the {@link StorageBucket} object to use for the storage provider lookup
+	 * @return the Single Observable containing the {@link StorageProvider} object for subscription
+	 */
+	StorageProvider getBackupStorageProvider(Long storageBucketId);
+
+	/**
+	 * Returns the {@link StorageProvider} for a specific storage bucket and base path.
+	 * @param storageBucketId the ID of the {@link StorageBucket} object to use for the storage provider lookup
+	 * @param basePath the base path to use for the storage provider
+	 * @return the Single Observable containing the {@link StorageProvider} object for subscription
+	 */
+	StorageProvider getBackupStorageProvider(Long storageBucketId, String basePath);
 	
 }
