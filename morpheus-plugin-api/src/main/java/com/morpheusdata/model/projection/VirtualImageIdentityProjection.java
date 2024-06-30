bb7941139657f4d8c0fa97d953c711730342f929
David Estes <davydotcom@gmail.com>
2024-06-30 10:43:23 -0400

    applying copyright notice PSL to top of files

--- morpheus-plugin-api/src/main/java/com/morpheusdata/model/projection/VirtualImageIdentityProjection.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/projection/VirtualImageIdentityProjection.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2024 Morpheus Data, LLC.
+ *
+ * Licensed under the PLUGIN CORE SOURCE LICENSE (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://raw.githubusercontent.com/gomorpheus/morpheus-plugin-core/v1.0.x/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.morpheusdata.model.projection;
 
 import com.morpheusdata.core.MorpheusVirtualImageService;
