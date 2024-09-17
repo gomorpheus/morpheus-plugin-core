b5a518469820cc36c284e7022d33f9548c2f548a
Dustin Deyoung <ddeyoung@morpheusdata.com>
2024-09-17 11:23:29 -0400

    update file copy java doc.

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/MorpheusSynchronousFileCopyService.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/synchronous/MorpheusSynchronousFileCopyService.java
@@ -11,7 +11,7 @@ public interface MorpheusSynchronousFileCopyService {
 	 * Copy a file to the target server.
 	 * @param server The target server
 	 * @param fileName name of the copied file
-	 * @param filePath path on the server to place the file copy.
+	 * @param filePath path on the server, including the file name (/some/path/file.txt), to place the file copy.
 	 * @param sourceStream source {@link InputStream} to copy to the server
 	 * @param contentLength size of the file to be copied
 	 * @return {@link ServiceResponse} containing the success status of the copy operation
