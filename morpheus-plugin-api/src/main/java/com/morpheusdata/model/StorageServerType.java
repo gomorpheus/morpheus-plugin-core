bb842dcebabbdc2f15ee5ba95d554bf49e1e802d
Alex Clement <MrClement@users.noreply.github.com>
2026-07-21 11:21:16 -0600

    Add shareOptionTypes to StorageServerType model

--- morpheus-plugin-api/src/main/java/com/morpheusdata/model/StorageServerType.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/StorageServerType.java
@@ -50,220 +50,220 @@ public class StorageServerType extends MorpheusModel {
 	// Collection<OptionType> groupOptionTypes;
 	Collection<StorageVolumeType> volumeTypes;
 	Collection<OptionType> bucketOptionTypes;
-	// Collection<OptionType> shareOptionTypes;
+	Collection<OptionType> shareOptionTypes;
 	// Collection<OptionType> shareAccessOptionTypes;
 
 	public String getCode() {
 		return code;
 	}
-	
+
 	public void setCode(String code) {
 		this.code = code;
 		markDirty("code", code);
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 		markDirty("name", name);
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public void setDescription(String description) {
 		this.description = description;
 		markDirty("description", description);
 	}
-	
+
 	public String getStorageService() {
 		return storageService;
 	}
-	
+
 	public void setStorageService(String storageService) {
 		this.storageService = storageService;
 		markDirty("storageService", storageService);
 	}
-	
+
 	public Boolean getEnabled() {
 		return enabled;
 	}
-	
+
 	public void setEnabled(Boolean enabled) {
 		this.enabled = enabled;
 		markDirty("enabled", enabled);
 	}
-	
+
 	public Boolean getCreatable() {
 		return creatable;
 	}
-	
+
 	public void setCreatable(Boolean creatable) {
 		this.creatable = creatable;
 		markDirty("creatable", creatable);
 	}
-	
+
 	public Boolean getHasNamespaces() {
 		return hasNamespaces;
 	}
-	
+
 	public void setHasNamespaces(Boolean hasNamespaces) {
 		this.hasNamespaces = hasNamespaces;
 		markDirty("hasNamespaces", hasNamespaces);
 	}
-	
+
 	public Boolean getHasGroups() {
 		return hasGroups;
 	}
-	
+
 	public void setHasGroups(Boolean hasGroups) {
 		this.hasGroups = hasGroups;
 		markDirty("hasGroups", hasGroups);
 	}
-	
+
 	public Boolean getHasBlock() {
 		return hasBlock;
 	}
-	
+
 	public void setHasBlock(Boolean hasBlock) {
 		this.hasBlock = hasBlock;
 		markDirty("hasBlock", hasBlock);
 	}
-	
+
 	public Boolean getHasObject() {
 		return hasObject;
 	}
-	
+
 	public void setHasObject(Boolean hasObject) {
 		this.hasObject = hasObject;
 		markDirty("hasObject", hasObject);
 	}
-	
+
 	public Boolean getHasFile() {
 		return hasFile;
 	}
-	
+
 	public void setHasFile(Boolean hasFile) {
 		this.hasFile = hasFile;
 		markDirty("hasFile", hasFile);
 	}
-	
+
 	public Boolean getHasDatastore() {
 		return hasDatastore;
 	}
-	
+
 	public void setHasDatastore(Boolean hasDatastore) {
 		this.hasDatastore = hasDatastore;
 		markDirty("hasDatastore", hasDatastore);
 	}
-	
+
 	public Boolean getHasDisks() {
 		return hasDisks;
 	}
-	
+
 	public void setHasDisks(Boolean hasDisks) {
 		this.hasDisks = hasDisks;
 		markDirty("hasDisks", hasDisks);
 	}
-	
+
 	public Boolean getHasHosts() {
 		return hasHosts;
 	}
-	
+
 	public void setHasHosts(Boolean hasHosts) {
 		this.hasHosts = hasHosts;
 		markDirty("hasHosts", hasHosts);
 	}
-	
+
 	public Boolean getCreateNamespaces() {
 		return createNamespaces;
 	}
-	
+
 	public void setCreateNamespaces(Boolean createNamespaces) {
 		this.createNamespaces = createNamespaces;
 		markDirty("createNamespaces", createNamespaces);
 	}
-	
+
 	public Boolean getCreateGroup() {
 		return createGroup;
 	}
-	
+
 	public void setCreateGroup(Boolean createGroup) {
 		this.createGroup = createGroup;
 		markDirty("createGroup", createGroup);
 	}
-	
+
 	public Boolean getCreateBlock() {
 		return createBlock;
 	}
-	
+
 	public void setCreateBlock(Boolean createBlock) {
 		this.createBlock = createBlock;
 		markDirty("createBlock", createBlock);
 	}
-	
+
 	public Boolean getCreateObject() {
 		return createObject;
 	}
-	
+
 	public void setCreateObject(Boolean createObject) {
 		this.createObject = createObject;
 		markDirty("createObject", createObject);
 	}
-	
+
 	public Boolean getCreateFile() {
 		return createFile;
 	}
-	
+
 	public void setCreateFile(Boolean createFile) {
 		this.createFile = createFile;
 		markDirty("createFile", createFile);
 	}
-	
+
 	public Boolean getCreateDatastore() {
 		return createDatastore;
 	}
-	
+
 	public void setCreateDatastore(Boolean createDatastore) {
 		this.createDatastore = createDatastore;
 		markDirty("createDatastore", createDatastore);
 	}
-	
+
 	public Boolean getCreateDisk() {
 		return createDisk;
 	}
-	
+
 	public void setCreateDisk(Boolean createDisk) {
 		this.createDisk = createDisk;
 		markDirty("createDisk", createDisk);
 	}
-	
+
 	public Boolean getCreateHost() {
 		return createHost;
 	}
-	
+
 	public void setCreateHost(Boolean createHost) {
 		this.createHost = createHost;
 		markDirty("createHost", createHost);
 	}
-	
+
 	public String getIconCode() {
 		return iconCode;
 	}
-	
+
 	public void setIconCode(String iconCode) {
 		this.iconCode = iconCode;
 		markDirty("iconCode", iconCode);
 	}
-	
+
 	public Boolean getHasFileBrowser() {
 		return hasFileBrowser;
 	}
-	
+
 	public void setHasFileBrowser(Boolean hasFileBrowser) {
 		this.hasFileBrowser = hasFileBrowser;
 		markDirty("hasFileBrowser", hasFileBrowser);
@@ -276,6 +276,15 @@ public class StorageServerType extends MorpheusModel {
 	public void setOptionTypes(Collection<OptionType> optionTypes) {
 		markDirty("optionTypes", this.optionTypes);
 		this.optionTypes = optionTypes;
+	}
+
+	public Collection<OptionType> getShareOptionTypes() {
+		return shareOptionTypes;
+	}
+
+	public void setShareOptionTypes(Collection<OptionType> shareOptionTypes) {
+		markDirty("shareOptionTypes", this.shareOptionTypes);
+		this.shareOptionTypes = shareOptionTypes;
 	}
 
 	public Collection<StorageVolumeType> getVolumeTypes() {
