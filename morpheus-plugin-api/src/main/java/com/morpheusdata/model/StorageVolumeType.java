22c30389dc8386db89f40c49125a84417c244fa5
Ron Saito <ron.saito@hpe.com>
2026-08-11 11:50:30 -0600

    rename new property to nativeVolume

--- morpheus-plugin-api/src/main/java/com/morpheusdata/model/StorageVolumeType.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/StorageVolumeType.java
@@ -58,8 +58,8 @@ public class StorageVolumeType extends MorpheusModel {
 	// Whether this volume type is physically tied to a specific server (for example
 	// local/RAID disks on bare metal). Defaults to false since most volume types
 	// (SAN/networked storage) can be reattached to different servers. Set to true
-	// for server-bound storage so instance reconfigure can ignore those types.
-	protected Boolean serverBound = false;
+	// for native storage so instance reconfigure can ignore those types.
+	protected Boolean nativeVolume = false;
 
 	// associations
 	Collection<OptionType> optionTypes;
@@ -299,11 +299,11 @@ public class StorageVolumeType extends MorpheusModel {
 		this.hasActiveReplica = hasActiveReplica;
 	}
 
-	public Boolean getServerBound() {
-		return serverBound;
+	public Boolean getNativeVolume() {
+		return nativeVolume;
 	}
 
-	public void setServerBound(Boolean serverBound) {
-		this.serverBound = serverBound;
+	public void setNativeVolume(Boolean nativeVolume) {
+		this.nativeVolume = nativeVolume;
 	}
 }
